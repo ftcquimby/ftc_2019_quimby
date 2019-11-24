53b803c2069ed1639babd233638e7ef9c9c840a3
nidhirg <nidhirg2014@gmail.com>
2019-11-24 01:28:56 -0800

    updated motionmethods so it works, added a foundation moving opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkOnly.java
@@ -1,25 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "StraightTest")
-//@Disabled
-public class StraightTest extends LinearOpMode {
+@Autonomous(name = "ParkOnly")
+public class ParkOnly extends LinearOpMode {
     OmegaBot robot;
+    OmegaPID drivePID;
     MotionMethods motionMethods;
 
     public void runOpMode(){
         robot = new OmegaBot(telemetry, hardwareMap);
+        drivePID = robot.drivePID;
         motionMethods = new MotionMethods(robot, telemetry, this);
 
         robot.drivetrain.setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.drivetrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
-
-        motionMethods.strafe(0, 200, 1);
+        motionMethods.moveMotionProfile(3,1);
     }
 }
